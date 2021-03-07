6c66a3393c08187160b8c5cc9df3d143b6f3b609
Alison A. Frank <allyfrank@gmail.com>
2021-03-07 14:07:43 -0500

    add interval
    Created an interval to call api every 20 seconds, display data to index.html

--- api_script.js
+++ api_script.js
@@ -2,6 +2,10 @@ var app = {
     coords: [],
     initialize: function() {
         app.getData();
+        setInterval(function(){
+          console.log("making call...");
+          app.getData();
+        }, 20000);
     },
     getData: function() {
         $.ajax({
@@ -11,25 +15,24 @@ var app = {
                 console.log(err);
             },
             success: function(data) {
-                console.log(data);
-                console.log(data['iss_position']);
-                //console.log("latitude: " + data["iss_position"]["latitude"]);
-                //console.log("longitude: " + data["iss_position"]["longitude"]);
-                app.coords[0] = data["iss_position"]["latitude"];
-                app.coords[1] = data["iss_position"]["longitude"];
+                //view json
+                //console.log(data);
 
-        
+                //store coordinates in coords array
+                app.coords[0] = parseFloat(data["iss_position"]["latitude"]);
+                app.coords[1] = parseFloat(data["iss_position"]["longitude"]);
+                //test to ensure values are captured
+                console.log(app.coords);
+                app.display();
             }
         });
-      
+    },
+    display: function() {
+      let htmlString = "<p>Latitutde: " + app.coords[0];
+      htmlString += " Longitude: " + app.coords[1] + "</p>";
+      $("#holder").html(htmlString);
     }
 };
-function startApp() {
+function startApp() { //call api from html
     app.initialize();
-}
-function appendData() {
-    let innertxt = "<p>Latitude: " + app.coords[0] + ", Longitude: " + app.coords[1] + "</p>";
-    let holder = document.createElement("div");
-    holder.id = 'holder';
-    holder.innerHTML = innertxt;
 }
